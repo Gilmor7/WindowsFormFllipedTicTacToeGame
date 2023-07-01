0d7263612dedb6c64993be4ac5b225a22292332b
gilm <gilm@radware.com>
2023-07-01 13:09:27 +0300

    start project and create base display for the Game settings screen

---
+++ WindowsFormsFlippedTicTacToe/FormSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsFlippedTicTacToe
+{
+    public partial class FormSettings : Form
+    {
+        public FormSettings()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
